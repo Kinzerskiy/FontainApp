ddb327c9eac201f615f9b02625340413002b1c53
Kinzerskiy <kinzzersky@gmail.com>
2023-06-06 18:07:27 +0300

    1. maps is working!

--- FontainApp/Flows/Maps/GoogleMapsViewController.swift
+++ FontainApp/Flows/Maps/GoogleMapsViewController.swift
@@ -9,52 +9,57 @@ import UIKit
 import GoogleMaps
 import UIKit
 import GoogleMaps
+import UIKit
+import GoogleMaps
 
 class GoogleMapsViewController: UIViewController {
-    
+
     @IBOutlet weak var mapView: UIView!
     @IBOutlet weak var pointInfoView: UIView!
-    
+
     var locationManager: CLLocationManager!
     var pointInfo: StoreInfoView?
-    
+    var googleMapView: GMSMapView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareUI()
-        
+
         if CLLocationManager.locationServicesEnabled() {
             DispatchQueue.main.async {
                 self.locationManager = CLLocationManager()
                 self.locationManager.delegate = self
                 self.locationManager.requestWhenInUseAuthorization()
-                
             }
         }
-        
+
         pointInfo?.roundCorners([.topLeft, .topRight], radius: 30)
     }
-    
+
     func prepareUI() {
+        pointInfoView.isHidden = true
         pointInfo = StoreInfoView.setup(in: pointInfoView)
     }
 }
 
 extension GoogleMapsViewController: CLLocationManagerDelegate {
-    
-  
+
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         let authorizationStatus = manager.authorizationStatus
 
         if authorizationStatus == .authorizedWhenInUse {
             DispatchQueue.main.async {
-                let camera = GMSCameraPosition.camera(withLatitude: 50.450526, longitude: 30.52797, zoom: 6.0)
-                let googleMapView = GMSMapView.map(withFrame: self.mapView.bounds, camera: camera)
+                let camera = GMSCameraPosition.camera(withLatitude: 50.450526, longitude: 30.52797, zoom: 15.0)
+                self.googleMapView = GMSMapView.map(withFrame: self.mapView.bounds, camera: camera)
+                self.googleMapView?.delegate = self
 
-                self.mapView.addSubview(googleMapView)
+                if let mapView = self.googleMapView {
+                    self.mapView.addSubview(mapView)
 
-                GoogleMapManager().getStoreLocations { locations in
-                    if let locations = locations {
-                        self.addMarkersToMap(with: locations, on: googleMapView)
+                    GoogleMapManager().getStoreLocations { locations in
+                        if let locations = locations {
+                            self.addMarkersToMap(with: locations, on: mapView)
+                        }
                     }
                 }
             }
@@ -65,12 +70,11 @@ extension GoogleMapsViewController: CLLocationManagerDelegate {
         }
     }
 
-    
     func addMarkersToMap(with locations: [Location], on mapView: GMSMapView) {
         for location in locations {
             let marker = GMSMarker()
-            
-            marker.position = CLLocationCoordinate2D(latitude: Double(location.latitude) , longitude: Double(location.longitude) )
+
+            marker.position = CLLocationCoordinate2D(latitude: Double(location.latitude), longitude: Double(location.longitude))
             marker.title = location.title
             marker.snippet = location.snippet
             marker.map = mapView
@@ -80,11 +84,18 @@ extension GoogleMapsViewController: CLLocationManagerDelegate {
             marker.tracksInfoWindowChanges = true
         }
     }
-    
-    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
-            guard let location = marker.userData as? Location else { return nil }
-            
-            pointInfo?.configure(with: location)
-            return pointInfo
-        }
+}
+
+extension GoogleMapsViewController: GMSMapViewDelegate {
+
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        guard let location = marker.userData as? Location else { return false }
+        pointInfo?.configure(with: location)
+        pointInfoView.isHidden = false
+        return false
+    }
+
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        pointInfoView.isHidden = true
+    }
 }
